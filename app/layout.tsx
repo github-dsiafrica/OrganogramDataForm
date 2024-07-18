43ef946a651b3bc02c7ba6d0fc46629cf4339317
Tino <tino.muzambi@uct.ac.za>
2024-07-18 13:55:31 +0200

    Adds nav.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<nav className="flex py-2 gap-8 justify-between px-4 items-center">
+					<Image
+						src="https://dsi-africa.org/sites/default/files/Logo%20-%20primary%20on%20white-%20transparent.png"
+						width={300}
+						height={200}
+						alt="DS-I Africa Logo"
+					/>
+					<h1 className="text-3xl font-bold">
+						DS-I Africa Organogram Data Generator
+					</h1>
+				</nav>
+				{children}
+			</body>
 		</html>
 	);
 }
