8a3e771a8cc4e18117bbcfaf5ba5274318cb0c58
Tino <tino.muzambi@uct.ac.za>
2024-07-18 14:03:02 +0200

    Uses remote patterns.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["dsi-africa.org"],
+		remotePatterns: [
+			{
+				hostname: "dsi-africa.org",
+			},
+		],
 	},
 };
 
