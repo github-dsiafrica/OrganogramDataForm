d8e6e1a063814de84b702999c5a8f017a8b8aa6d
Tino <tino.muzambi@uct.ac.za>
2024-07-18 15:11:48 +0200

    Reads in data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
-import Image from "next/image";
+"use client";
+
+import Papa from "papaparse";
+import { FormEventHandler } from "react";
 
 export default function Home() {
-	return <main></main>;
+	const handleFile: FormEventHandler<HTMLFormElement> = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target as HTMLFormElement);
+
+		const file = formData.get("csv") as File;
+		Papa.parse(file, {
+			header: true,
+			complete: (result) => {
+				console.log(result);
+			},
+		});
+	};
+
+	return (
+		<main>
+			<form onSubmit={handleFile}>
+				<input type="file" name="csv" id="csv" />
+				<button type="submit">Submit</button>
+			</form>
+		</main>
+	);
 }
