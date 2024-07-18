b6b940952ec2c215b3bb6442611a1812d3140e03
Tino <tino.muzambi@uct.ac.za>
2024-07-18 16:38:27 +0200

    Adds switch for row type.

--- components/TableRow.tsx
+++ components/TableRow.tsx
@@ -1,6 +1,31 @@
 import { TableRowProps } from "@/interfaces";
 
 const TableRow: React.FC<TableRowProps> = ({ type, row }): JSX.Element => {
-	return <div className="">{row.id}</div>;
+	switch (row.type) {
+		case "group":
+			return (
+				<div className="">
+					{row.id} {row.type}
+				</div>
+			);
+		case "project":
+			return (
+				<div className="">
+					{row.id} {row.type}
+				</div>
+			);
+		case "member":
+			return (
+				<div className="">
+					{row.id} {row.type}
+				</div>
+			);
+		case "info":
+			return (
+				<div className="">
+					{row.id} {row.type}
+				</div>
+			);
+	}
 };
 export default TableRow;
