009f635e3d323ae4a7af1dd9ff8460361f4c4935
Tino <tino.muzambi@uct.ac.za>
2024-07-24 11:03:56 +0200

    Uses enum for role.

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -26,52 +26,54 @@ type Member = {
 	title: string;
 	picture?: string;
 	type: "member";
-	role:
-		| "Contact PI"
-		| "MPI"
-		| "PI"
-		| "Co-PI"
-		| "Program Manager"
-		| "Project Manager/Coordinator"
-		| "Project Coordinator"
-		| "Administrator"
-		| "Administrative Assistant"
-		| "Collaborator"
-		| "Collaboration"
-		| "Consultant"
-		| "Co-Investigator"
-		| "DMAC Lead/Member"
-		| "Data Analyst"
-		| "Data curator"
-		| "Data Manager"
-		| "Data scientist"
-		| "eLwazi Uganda Node Bioinformatician/Data Scientist"
-		| "Hub Deputy Director"
-		| "Master's student"
-		| "Member and co-chair I-WG"
-		| "Member/Collaborator"
-		| "PhD Student"
-		| "PhD Trainee"
-		| "Post-Doc"
-		| "Pre-Doc"
-		| "Project Team Member"
-		| "REDCap Administrator"
-		| "REDCap Database Developer"
-		| "Research Assistant"
-		| "Research Fellow"
-		| "Researcher"
-		| "Sequencing"
-		| "Site Investigator"
-		| "Site PI"
-		| "Software Developer"
-		| "Software Engineer"
-		| "System Admin | Data Scientist"
-		| "Systems Administrator"
-		| "Training and Outreach Coordinator"
-		| "Training Coordinator"
-		| "Web Developer"
-		| "";
+	role: Role;
 };
+
+export enum Role {
+	ContactPI = "Contact PI",
+	MPI = "MPI",
+	PI = "PI",
+	CoPI = "Co-PI",
+	ProgramManager = "Program Manager",
+	ProjectManagerCoordinator = "Project Manager/Coordinator",
+	ProjectCoordinator = "Project Coordinator",
+	Administrator = "Administrator",
+	AdministrativeAssistant = "Administrative Assistant",
+	Collaborator = "Collaborator",
+	Collaboration = "Collaboration",
+	Consultant = "Consultant",
+	CoInvestigator = "Co-Investigator",
+	DMACLeadMember = "DMAC Lead/Member",
+	DataAnalyst = "Data Analyst",
+	DataCurator = "Data curator",
+	DataManager = "Data Manager",
+	DataScientist = "Data scientist",
+	eLwaziUgandaNodeBioinformaticianDataScientist = "eLwazi Uganda Node Bioinformatician/Data Scientist",
+	HubDeputyDirector = "Hub Deputy Director",
+	MastersStudent = "Master's student",
+	MemberAndCoChairIWG = "Member and co-chair I-WG",
+	MemberCollaborator = "Member/Collaborator",
+	PhDStudent = "PhD Student",
+	PhDTrainee = "PhD Trainee",
+	PostDoc = "Post-Doc",
+	PreDoc = "Pre-Doc",
+	ProjectTeamMember = "Project Team Member",
+	REDCapAdministrator = "REDCap Administrator",
+	REDCapDatabaseDeveloper = "REDCap Database Developer",
+	ResearchAssistant = "Research Assistant",
+	ResearchFellow = "Research Fellow",
+	Researcher = "Researcher",
+	Sequencing = "Sequencing",
+	SiteInvestigator = "Site Investigator",
+	SitePI = "Site PI",
+	SoftwareDeveloper = "Software Developer",
+	SoftwareEngineer = "Software Engineer",
+	SystemAdminDataScientist = "System Admin | Data Scientist",
+	SystemsAdministrator = "Systems Administrator",
+	TrainingAndOutreachCoordinator = "Training and Outreach Coordinator",
+	TrainingCoordinator = "Training Coordinator",
+	WebDeveloper = "Web Developer",
+}
 
 type Info = {
 	id: string;
